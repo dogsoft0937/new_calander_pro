8fb60b476974611a82e314e567f594c1fe4c1b01
dogsoft0937 <dabin0450@naver.com>
2020-04-20 00:01:30 +0900

    implement calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import '../styles/Calendar.scss';
 import moment, { Moment as MomentTypes } from 'moment';
 
 function Calendar() {
-  function generate() {
-    const today = moment();
-    const startWeek = today.clone().startOf('month').week();
-    const endWeek = today.clone().endOf('month').week() === 1 ? 53 : today.clone().endOf('month').week();
+  function generate(params=0) {
+    let today=""
+    if(params>=0){
+      today = moment().add(params,'months');
+    }else{
+      today = moment().subtract(Math.abs(params),'months');
+    }
+    const startWeek = today.clone().startOf('month').week(); //현재 달의 시작 주 
+    const endWeek = today.clone().endOf('month').week() === 1 ? 53 : today.clone().endOf('month').week(); //현재 달의 마지막 주
     let calendar = [];
     for (let week = startWeek; week <= endWeek; week++) {
       calendar.push(
@@ -15,7 +20,9 @@ function Calendar() {
             Array(7).fill(0).map((n, i) => {
               let current = today.clone().week(week).startOf('week').add(n + i, 'day')
               let isSelected = today.format('YYYYMMDD') === current.format('YYYYMMDD') ? 'selected' : '';
+
               let isGrayed = current.format('MM') === today.format('MM') ? '' : 'grayed';
+            
               return (
                 <div className={`box  ${isSelected} ${isGrayed}`} key={i}>
                   <span className={`text`}>{current.format('D')}</span>
@@ -28,10 +35,26 @@ function Calendar() {
     }
     return calendar;
   }
+  const [params,setParams]=useState(0);
+  const [test,setTest]=useState(generate());
+  const [current,setCurrent]=useState(moment().format('YYYY-MM'));
+  const changeDate=(e)=>{
+    if(e.target.name==="pre"){
+      setParams(params-1);
+      setTest(generate(params));
+      setCurrent(moment().subtract(Math.abs(params),'months').format('YYYY-MM'));
+    }else{
+      setParams(params+1);
+      setTest(generate(params));
+      setCurrent(moment().add(params,'months').format('YYYY-MM'));
+    }
+  }
   return (
     <div className="Calendar">
       <div className="head">
-        <span className="title">{moment().format('YYYY-MM-DD')}</span>
+      <button onClick={changeDate} name="pre">전</button>
+      <span className="title">{current}</span>  
+      <button onClick={changeDate} name="nex">후</button>
       </div>
       <div className="body">
         <div className="row">
@@ -57,7 +80,7 @@ function Calendar() {
             <span className="text">토</span>
           </div>
         </div>
-        {generate()}
+        {test}
       </div>
     </div>
   )
